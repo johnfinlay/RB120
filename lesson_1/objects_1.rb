d01d428edfb0932806294da18318fc2226d99df8
John Finlay <finlay_john@hotmail.com>
2020-10-11 08:19:02 -0500

    objects 1 exercise 1

--- lesson_1/objects_1.rb
+++ lesson_1/objects_1.rb
@@ -1,7 +1,37 @@
-class GoodDog
-  def initialize
-    puts 'This object was initialized!'
+# Exercise 1
+class MyCar
+  attr_accessor :year, :color, :model, :speed
+
+  def initialize(y, c, m)
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def increase_speed(mph)
+    @speed += mph
+  end
+
+  def brake(mph)
+    @speed -= mph
+  end
+
+  def shut_off
+    @speed = 0
   end
 end
 
-sparky = GoodDog.new
+jimmy = MyCar.new(1979, 'grey', 'GMC Jimmy')
+jimmy.increase_speed(55)
+puts jimmy.speed
+jimmy.brake(25)
+puts jimmy.speed
+jimmy.shut_off
+puts jimmy.speed
+
+# Exercise 2
+
+
+
+#Exercise 3
