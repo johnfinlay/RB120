3e1771cba81c1a64923394eda6eefa75e1045bc7
John Finlay <finlay_john@hotmail.com>
2020-10-12 06:24:19 -0500

    classes & objects II exercises

--- lesson_1/my_car.rb
+++ lesson_1/my_car.rb
@@ -9,6 +9,14 @@ class MyCar
     @speed = 0
   end
 
+  def to_s
+    "You've got a #{self.color} #{self.year} #{self.model} running #{self.speed} mph!"
+  end
+
+  def self.calculate_mileage(miles, gallons)
+    puts "#{miles / gallons} miles per gallon."
+  end
+
   def increase_speed(mph)
     @speed += mph
   end
@@ -31,7 +39,11 @@ jimmy.increase_speed(55)
 puts jimmy.speed
 jimmy.brake(25)
 puts jimmy.speed
-jimmy.shut_off
-puts jimmy.speed
+# jimmy.shut_off
+# puts jimmy.speed
 jimmy.spray_paint('white')
 puts jimmy.color
+
+MyCar.calculate_mileage(351, 13)
+
+puts jimmy
