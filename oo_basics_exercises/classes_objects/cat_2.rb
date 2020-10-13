a567fd5216ec3208b08765c875f17001d6ccaafd
John Finlay <finlay_john@hotmail.com>
2020-10-13 07:01:37 -0500

    final cat_2 exercise

--- oo_basics_exercises/classes_objects/cat_2.rb
+++ oo_basics_exercises/classes_objects/cat_2.rb
@@ -1,33 +1,14 @@
 class Cat
-  attr_accessor :name
-  @@total_cats = 0
-  COLOR = 'gray'
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @@total_cats += 1
   end
 
-  def rename(new_name)
-    self.name = new_name
-  end
-
-  def identify
-    self
-  end
-
-  def self.generic_greeting
-    puts "A cat I am."
-  end
-  
-  def greet
-    puts "Hello! My name is #{name} and I'm a #{COLOR} cat!"
-  end
-
-  def self.total
-    puts @@total_cats
+  def to_s
+    "I'm #{name}!"
   end
 end
 
 kitty = Cat.new('Sophie')
-kitty.greet
+puts kitty
