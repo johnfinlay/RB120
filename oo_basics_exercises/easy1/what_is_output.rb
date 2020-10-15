b5670c23dd3a821e34b4859a4fafcd9aaf6c4246
John Finlay <finlay_john@hotmail.com>
2020-10-15 07:14:10 -0500

    What's the output exercise further exploration

--- oo_basics_exercises/easy1/what_is_output.rb
+++ oo_basics_exercises/easy1/what_is_output.rb
@@ -10,9 +10,20 @@ class Pet
   end
 end
 
-name = 'Fluffy'
+name = 42
 fluffy = Pet.new(name)
+name += 1
 puts fluffy.name
 puts fluffy
 puts fluffy.name
 puts name
+
+# The to_s call in Pet#initialize returns
+# a new String object that is assigned to @name
+# The reassignment of the local variable name
+# on line 15 does not affect the @name object
+
+# If the local variable name is already a string,
+# then the original String object will be assigned to @name.
+# That is why the original #upcase! call modified
+# the local variable name in the original exercise.
