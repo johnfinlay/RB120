749406544705f7f93f861b27db3ab328bb08d0cb
John Finlay <finlay_john@hotmail.com>
2020-10-17 06:36:52 -0500

    books part 2

--- oo_basics_exercises/easy1/book.rb
+++ oo_basics_exercises/easy1/book.rb
@@ -1,17 +1,14 @@
 class Book
-  attr_reader :author, :title
-  
-  def initialize(author, title)
-    @author = author
-    @title = title
-  end
+  attr_accessor :title, :author
 
   def to_s
     %("#{title}", by #{author})
   end
 end
 
-book = Book.new("Neil Stephenson", "Snow Crash")
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
 puts %(The author of "#{book.title}" is #{book.author}.)
 puts %(book = #{book}.)
 
