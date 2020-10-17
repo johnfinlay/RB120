2a96fbf0812eef893f2e833abbbc1040ce2f4a62
John Finlay <finlay_john@hotmail.com>
2020-10-17 06:59:08 -0500

    complete the program - cats! solution

--- oo_basics_exercises/easy1/cats.rb
+++ oo_basics_exercises/easy1/cats.rb
@@ -1,16 +1,19 @@
 class Pet
-  def initialize(name, age, color)
+  def initialize(name, age)
     @name = name
     @age = age
-    @color = color
-  end
-
-  def to_s
-    "My cat #{@name} is #{@age} years old and has #{@color} fur."
   end
 end
 
 class Cat < Pet
+  def initialize(name, age, colors)
+    super(name, age)
+    @colors = colors
+  end
+
+  def to_s
+    "My cat #{@name} is #{@age} years old and has #{@colors} fur."
+  end
 end
 
 pudding = Cat.new('Pudding', 7, 'black and white')
